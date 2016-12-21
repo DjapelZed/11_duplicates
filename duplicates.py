56b9099f586c7853d782a37d584edabc5ffe1565
DjapelZed <maks.kushnirov@gmail.com>
2016-12-21 22:08:28 +0300

    Упрощена функция print_duplicates

--- duplicates.py
+++ duplicates.py
@@ -3,12 +3,17 @@ import os
 
 def get_files_in_path(path):
     files_list = list()
-    for entry in os.scandir(path):
-        if entry.is_file(follow_symlinks=False):
-            file_statistic = (entry.name, entry.stat().st_size, entry.path)
-            files_list.append(file_statistic)
-        elif entry.is_dir(follow_symlinks=False):
-            files_list.extend(get_files_in_path(entry.path))
+    try:
+        for entry in os.scandir(path):
+            if entry.is_file(follow_symlinks=False):
+                file_statistic = (entry.name, entry.stat().st_size, entry.path)
+                files_list.append(file_statistic)
+            elif entry.is_dir(follow_symlinks=False):
+                files_list.extend(get_files_in_path(entry.path))
+    except PermissionError:
+        print('Permission error!')
+    except FileNotFoundError:
+        print('Path not found!')
     return files_list
 
 
@@ -34,20 +39,16 @@ def get_duplicates(path):
 
 
 def print_duplicates(path):
-    try:
-        duplicates = get_duplicates(path)
-        if duplicates:
-            for block_of_duplicates in duplicates:
-                print('Name:', block_of_duplicates[0][1], 'Size:', block_of_duplicates[0][2], 'byte')
-                print('Duplicates:')
-                for duplicate in block_of_duplicates:
-                    print(duplicate[0])
-    except PermissionError:
-        print('Permission error!')
-    except FileNotFoundError:
-        print('Path not found!')
-    input('Press enter')
+    duplicates = get_duplicates(path)
+    if duplicates:
+        for block_of_duplicates in duplicates:
+            print('Name:', block_of_duplicates[0][1], 'Size:', block_of_duplicates[0][2], 'byte')
+            print('Duplicates:')
+            for duplicate in block_of_duplicates:
+                print(duplicate[0])
+
 
 if __name__ == '__main__':
     user_path = input('Directory path: ')
     print_duplicates(user_path)
+    input('Press Enter')
